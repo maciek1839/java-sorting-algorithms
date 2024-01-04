bb8c389930c26c1c7777079b2ee24f9a55c12ea7
Maciek <maciek1839@gmail.com>
2024-01-04 15:13:17 +1100

    changes

--- src/test/groovy/com/showmeyourcode/projects/algorithms/benchmark/BenchmarkProcessorTest.groovy
+++ src/test/groovy/com/showmeyourcode/projects/algorithms/benchmark/BenchmarkProcessorTest.groovy
@@ -1,28 +1,28 @@
-//package com.showmeyourcode.projects.algorithms.benchmark
-//
-//import com.showmeyourcode.projects.algorithms.exception.BenchmarkDataNotFoundException
-//import com.showmeyourcode.projects.algorithms.test_util.TestBase
-//
-//class BenchmarkProcessorTest extends TestBase {
-//
-//    def "should generate a report for algorithms without errors when the rest throw exceptions"() {
-//        given:
-//        def dataGenerator = Mock(BenchmarkDataGenerator.class)
-//        dataGenerator.loadData(_ as BenchmarkData) >> { BenchmarkData data ->
-//            if (data == AlgorithmsBenchmarkData.FIFTY_THOUSAND) {
-//                return new int[]{5, 32, 4, 21, 7}
-//            } else {
-//                throw new BenchmarkDataNotFoundException(data)
-//            }
-//        }
-//        def benchmarkProcessor = new BenchmarkProcessor(dataGenerator, applicationConfiguration)
-//
-//        when:
-//        def report = benchmarkProcessor.getBenchmarkDataReport()
-//
-//        then:
-//        report.partialResults().size() == 1
-//        and:
-//        report.partialResults().stream().mapToInt(group -> group.results.size()).sum() == 9
-//    }
-//}
+package com.showmeyourcode.projects.algorithms.benchmark
+
+import com.showmeyourcode.projects.algorithms.exception.BenchmarkDataNotFoundException
+import com.showmeyourcode.projects.algorithms.test_util.TestBase
+
+class BenchmarkProcessorTest extends TestBase {
+
+    def "should generate a report for algorithms without errors when the rest throw exceptions"() {
+        given:
+        def dataGenerator = Mock(BenchmarkDataGenerator.class)
+        dataGenerator.loadData(_ as BenchmarkData) >> { BenchmarkData data ->
+            if (data == AlgorithmsBenchmarkData.FIFTY_THOUSAND) {
+                return new int[]{5, 32, 4, 21, 7}
+            } else {
+                throw new BenchmarkDataNotFoundException(data)
+            }
+        }
+        def benchmarkProcessor = new BenchmarkProcessor(dataGenerator, applicationConfiguration)
+
+        when:
+        def report = benchmarkProcessor.getBenchmarkDataReport()
+
+        then:
+        report.partialResults().size() == 1
+        and:
+        report.partialResults().stream().mapToInt(group -> group.results.size()).sum() == 9
+    }
+}
